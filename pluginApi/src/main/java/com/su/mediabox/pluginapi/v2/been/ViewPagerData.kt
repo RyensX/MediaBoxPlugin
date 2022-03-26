841b202fe061e43ed5f0164eead07bfc01856c0b
RyensX <ryensu@qq.com>
2022-03-26 22:26:06 +0800

    [optimize]优化多页面视图组件页面名称及支持设置默认页

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/v2/been/ViewPagerData.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/v2/been/ViewPagerData.kt
@@ -3,18 +3,20 @@ package com.su.mediabox.pluginapi.v2.been
 /**
  * 多页面视图组件数据，类似ViewPager(2)
  * @param pages 推荐使用同一个实例，然后通过page控制加载内容
+ * @param defaultPage 默认打开的页，默认0
  */
 //TODO 更多自定义属性
 class ViewPagerData(
-    val pageLoaders: List<PageLoader>
+    val pageLoaders: List<PageLoader>,
+    val defaultPage: Int = 0
 ) : BaseData() {
     interface PageLoader {
 
-        val pageName: String
+        fun pageName(page: Int): String
 
         /**
          * 加载数据
-         * @param page 页码，从0开始
+         * @param page 页码(Tab的index)，从0开始
          */
         suspend fun loadData(page: Int): List<BaseData>
     }
