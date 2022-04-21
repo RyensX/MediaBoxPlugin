3e18c54330c858f1c9c4568ccb4d7d1a2e74bd15
RyensX <ryensu@qq.com>
2022-04-21 20:13:03 +0800

    [optimize]移除无用代码

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/AppUtil.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/AppUtil.kt
@@ -7,25 +7,11 @@ object AppUtil {
     var appContext: Context? = null
         private set
 
-    var appProcessor: IRouteProcessor? = null
-        private set
-
     /**
      * 由App初始化
      */
-    fun init(applicationContext: Context, appProcessor: IRouteProcessor) {
+    fun init(applicationContext: Context) {
         appContext = applicationContext
-        this.appProcessor = appProcessor
-    }
-
-    interface IRouteProcessor {
-        /**
-         * 处理根据actionUrl跳转
-         *
-         * @param actionUrl 跳转路径
-         * @return 成功处理了跳转，则返回true，没处理则返回false
-         */
-        fun process(actionUrl: String): Boolean
     }
 
 }
