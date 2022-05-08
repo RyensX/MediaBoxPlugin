42aa590ce3aaa8e8084f9e93d6a41910c8afc78e
RyensX <ryensu@qq.com>
2022-05-08 20:24:21 +0800

    [feature]媒体信息样式1视图组件支持控制封面高度及其他数据颜色

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/data/MediaInfo1Data.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/data/MediaInfo1Data.kt
@@ -5,12 +5,15 @@ import com.su.mediabox.pluginapi.util.UIUtil.dp
 /**
  * 媒体信息样式1视图组件数据
  * @param other 剧集信息等
+ * @param otherColor 默认-1使用主题色
  */
 data class MediaInfo1Data(
     val name: String,
     val coverUrl: String,
     val url: String,
-    val other: String = ""
+    val other: String = "",
+    val otherColor: Int = -1,
+    val coverHeight: Int = 150.dp
 ) : BaseData() {
 
     override var spanSize: Int = 2
