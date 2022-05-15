96f59be11732a26f17b2d36f0e7743eb399a8173
RyensX <ryensu@qq.com>
2022-05-15 13:34:27 +0800

    [feature]PlayAction支持自定义解码器

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/action/PlayAction.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/action/PlayAction.kt
@@ -2,15 +2,26 @@ package com.su.mediabox.pluginapi.action
 
 import android.content.Context
 
+/**
+ * @param playerManager 自定义解码器(Icom.shuyu.gsyvideoplayer.player.PlayerManager)，为空则使用默认的
+ */
 class PlayAction private constructor(
     val episodeUrl: String,
     var coverUrl: String,
     var detailPartUrl: String,
-    var videoName: String
+    var videoName: String,
+    var playerManager: Class<*>? = null
 ) : Action() {
     companion object {
         //由宿主初始化
         lateinit var GO: PlayAction.(Context) -> Unit
+
+        fun obtain(
+            episodeUrl: String,
+            coverUrl: String = "",
+            detailPartUrl: String = "",
+            videoName: String = ""
+        ) = obtain(episodeUrl, coverUrl, detailPartUrl, videoName, null)
 
         /**
          * @param episodeUrl 剧集链接，插件只需要提供此参数，其余数据由宿主自动填入
@@ -19,9 +30,11 @@ class PlayAction private constructor(
             episodeUrl: String,
             coverUrl: String = "",
             detailPartUrl: String = "",
-            videoName: String = ""
+            videoName: String = "",
+            playerManager: Class<*>?
         ) = getAction {
-            this.episodeUrl == episodeUrl
+            this.episodeUrl == episodeUrl &&
+                    this.playerManager == playerManager
         }
             ?: PlayAction(episodeUrl, coverUrl, detailPartUrl, videoName)
     }
