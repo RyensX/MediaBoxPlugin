623b4a50c7d7491d9f0da3890adf12b0eebefbf5
RyensX <ryensu@qq.com>
2022-05-31 14:28:16 +0800

    [feature]水平列表视图组件支持限制item宽度

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/data/HorizontalListData.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/data/HorizontalListData.kt
@@ -1,6 +1,13 @@
 package com.su.mediabox.pluginapi.data
 
+import android.view.ViewGroup
+import com.su.mediabox.pluginapi.util.UIUtil.dp
+
 /**
  * 水平列表视图组件数据
+ * @param widthLimit 宽度限制，可设置dp和[ViewGroup.LayoutParams.MATCH_PARENT]等
  */
-class HorizontalListData(val listData: List<BaseData>) : BaseData()
+class HorizontalListData @JvmOverloads constructor(
+    val listData: List<BaseData>,
+    val itemWidthLimit: Int = 128.dp
+) : BaseData()
