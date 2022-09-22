b09dee1847731c0fa91879c84cb18b48e662ed0a
RyensX <ryensu@qq.com>
2022-09-22 23:55:22 +0800

    [feature]PlayAction新增autoRotate以支持控制自动旋转

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/action/PlayAction.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/action/PlayAction.kt
@@ -4,13 +4,15 @@ import android.content.Context
 
 /**
  * @param playerManager 自定义解码器(Icom.shuyu.gsyvideoplayer.player.PlayerManager)，为空则使用默认的
+ * @param autoRotate 开始播放时是否支持自动旋转
  */
 class PlayAction private constructor(
     val episodeUrl: String,
     var coverUrl: String,
     var detailPartUrl: String,
     var videoName: String,
-    var playerManager: Class<*>? = null
+    var playerManager: Class<*>?,
+    val autoRotate: Boolean
 ) : Action() {
     companion object {
         //由宿主初始化
@@ -31,12 +33,13 @@ class PlayAction private constructor(
             coverUrl: String = "",
             detailPartUrl: String = "",
             videoName: String = "",
-            playerManager: Class<*>?
+            playerManager: Class<*>? = null,
+            autoRotate: Boolean = false
         ) = getAction {
             this.episodeUrl == episodeUrl &&
                     this.playerManager == playerManager
         }
-            ?: PlayAction(episodeUrl, coverUrl, detailPartUrl, videoName)
+            ?: PlayAction(episodeUrl, coverUrl, detailPartUrl, videoName, playerManager, autoRotate)
     }
 
     override fun go(context: Context) {
