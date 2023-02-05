8dea02c49f68129872edd61435885aab64c69bc8
RyensX <ryensu@qq.com>
2023-02-05 18:48:52 +0800

    [optimize]WebBrowserAction支持设置LoadPolicy

--- pluginApi/src/main/java/com/su/mediabox/pluginapi/action/WebBrowserAction.kt
+++ pluginApi/src/main/java/com/su/mediabox/pluginapi/action/WebBrowserAction.kt
@@ -1,13 +1,17 @@
 package com.su.mediabox.pluginapi.action
 
 import android.content.Context
+import com.su.mediabox.pluginapi.util.WebUtil
 
-class WebBrowserAction private constructor(val url: String) : Action() {
+class WebBrowserAction private constructor(val url: String, val loadPolicy: WebUtil.LoadPolicy) :
+    Action() {
     companion object {
         //由宿主初始化
         lateinit var GO: WebBrowserAction.(Context) -> Unit
 
-        fun obtain(url: String) = getAction { this.url == url } ?: WebBrowserAction(url)
+        @JvmOverloads
+        fun obtain(url: String, loadPolicy: WebUtil.LoadPolicy = WebUtil.DefaultLoadPolicy) =
+            getAction { this.url == url } ?: WebBrowserAction(url, loadPolicy)
     }
 
     override fun go(context: Context) {
